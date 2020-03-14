010acb3ae1a8658c508fbbe7347ad59250dca1eb
muj-programmer <nick.muj.programmer@gmail.com>
2020-03-14 12:33:38 +0530

    initial commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Welcome"),
+          ],
+        ),
+      ),
+    );
+  }
+}

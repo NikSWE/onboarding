f0521ed59a7ad92a1b7e28f13a49f6e642bd2765
muj-programmer <nick.muj.programmer@gmail.com>
2020-03-14 15:55:32 +0530

    complete onboarding screen

---
+++ lib/widgets/onboarding_page_1.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:onboarding/constants/text_styles.dart';
+
+class OnboardingPage1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(40.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Center(
+            child: Image(
+              image: AssetImage('assets/images/onboarding_1.png'),
+              height: 300.0,
+              width: 300.0,
+            ),
+          ),
+          SizedBox(height: 30.0),
+          Text(
+            'Live your life smarter\nwith us!',
+            style: kTitleStyle,
+          ),
+          SizedBox(height: 15.0),
+          Text(
+            'Anim labore pariatur deserunt culpa veniam. Ex ipsum sunt occaecat aliquip irure enim ut aliqua qui incididunt non adipisicing aute.',
+            style: kSubtitleStyle,
+          ),
+        ],
+      ),
+    );
+  }
+}
